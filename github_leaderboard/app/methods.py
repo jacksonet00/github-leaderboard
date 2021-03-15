f24e08c3a9b23eb4525a15c67fecf71f510c3d7c
leeg8 <leeg1@ufl.edu>
2021-03-15 11:34:48 +0530

    Paging is added to commit fetch process. now all, pages are fetch iteratively. also, process exit logic implemented. so, during fetching, if we found a commit that already exists on our system then we stop process because github returns commits sorted by timestamp from latest to older

--- github_leaderboard/app/methods.py
+++ github_leaderboard/app/methods.py
@@ -6,6 +6,22 @@ from . import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
+'''
+given a link header from github, find the link for the next url which they use for pagination
+'''
+def find_next(link):
+    for l in link.split(','):
+        a, b = l.split(';')
+        if b.strip() == 'rel="next"':
+            return a.strip()[1:-1]
+
+def is_obj_in_page(o, page):
+    for obj in page:
+        if(obj['node_id'] == o.nodeid ):
+            return True
+    return False
+
 def refresh_leaderboard_commits(id):
     leaderboard = get_object_or_404(models.Leaderboard, id=id)
     url_str = leaderboard.repo_url.replace('https://github.com/','').strip("/")
@@ -15,13 +31,43 @@ def refresh_leaderboard_commits(id):
     commits_url = 'https://api.github.com/repos/'+ url_str + '/commits'
     # commits_url = 'https://api.github.com/repos/jacksonet00/github-leaderboard/commits'
     
-    r=requests.get(commits_url,auth=(user, token))
+    commits = models.CommitRecord.objects.all().order_by('-timestamp')
+    if(commits.exists()):
+        latest_commit = commits[0]
+        print(latest_commit)
+    else:
+        latest_commit=None
 
-    l = r.json()
-    print(l)
+    l=[]
+    next_url = commits_url
+    while(True):
+        r=requests.get(next_url,auth=(user, token))
+        page = r.json()
+
+        l.extend(page)  # Add objects from page to our list
+        
+        # Github returns commits ordered by latest timestamp. 
+        # So, if page contains latest commit present on our system, then stop process
+        # because, later commits must be already present in our system
+        if(latest_commit):
+            if( is_obj_in_page(latest_commit, page) ):
+                break
+
+        n = len(page)
+        print(n)
+        if n == 0:
+            break
+        link = r.headers.get('link')
+        if link is None:
+            break
+        next_url = find_next(r.headers['link'])
+        if next_url is None:
+            break
+    
+    # print(l[:2])
     updated = 0
     if(r.status_code == 200):
-        print(l[0])
+        # print(l[0])
         for x in l:
             if(models.CommitRecord.objects.filter(nodeid=x['node_id']).exists() ):
                 continue # skip if commit object already exists
