40c11267216f8155fbfd469fb0a899386f0d28cb
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-03-17 10:54:22 -0400

    fix model

--- github_leaderboard/app/models.py
+++ github_leaderboard/app/models.py
@@ -4,11 +4,11 @@ import datetime as dt
 
 # Create your models here.
 class Result(models.Model):
-    def default_score(self):
+    def default_score():
         return 0
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.IntegerField(default=self.default_score)
+    score = models.IntegerField(default=default_score)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -16,15 +16,15 @@ class Result(models.Model):
         return f"{self.user}: {self.score} points"
 
 class Leaderboard(models.Model):
-    def default_start_datetime(self):
+    def default_start_datetime():
         return dt.datetime.now()
 
-    def default_end_datetime(self):
+    def default_end_datetime():
         return dt.datetime.now() + dt.timedelta(days=7)
 
     name = models.CharField(max_length=255)
-    start = models.DateTimeField(default=self.default_start_datetime)
-    end = models.DateTimeField(default=self.default_end_datetime)
+    start = models.DateTimeField(default=default_start_datetime)
+    end = models.DateTimeField(default=default_end_datetime)
     repo_url = models.URLField(blank=True, null=True)
     owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     results = models.ForeignKey(Result, blank=True, null=True, on_delete=models.CASCADE)
@@ -35,11 +35,11 @@ class Leaderboard(models.Model):
         return self.name
 
 class Commit(models.Model):
-    def default_commit_count(self):
+    def default_commit_count():
         return 0
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    commit_count = models.IntegerField(default=self.default_commit_count)
+    commit_count = models.IntegerField(default=default_commit_count)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
