4fb38856fa1a49a306abe3164f9df88def94d00f
Matthew Parks <matthewparks373@gmail.com>
2021-04-07 19:28:56 -0400

    fixed trailing whitespace

--- github_leaderboard/app/forms.py
+++ github_leaderboard/app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from github_leaderboard.app.models import Leaderboard
 
 class CreateLeaderboardForm(forms.ModelForm):
-    
+
     class Meta:
         model = Leaderboard
         fields = ('name', 'repo_url', 'end', 'start')
