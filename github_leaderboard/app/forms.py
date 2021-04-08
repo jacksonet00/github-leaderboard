42ae451a791d7041527be495a4f9f084662d991f
Matthew Parks <matthewparks373@gmail.com>
2021-04-08 00:40:43 -0400

    cleaned up the forms for dashboard

--- github_leaderboard/app/forms.py
+++ github_leaderboard/app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Column
 
 from github_leaderboard.app.models import Leaderboard
 
@@ -7,3 +9,15 @@ class CreateLeaderboardForm(forms.ModelForm):
     class Meta:
         model = Leaderboard
         fields = ("name", "repo_url", "end", "start")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Column("name", css_class="form-group col-md-6 mb-0"),
+            Column("repo_url", css_class="form-group col-md-6 mb-0"),
+            Column("end", css_class="form-group col-md-6 mb-0"),
+            Column("start", css_class="form-group col-md-6 mb-0"),
+        )
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Create Leaderboard"))
