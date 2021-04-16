82eb3c9147ee4a2fdf3e648c770288b05903e7c7
awhigham9 <awhigham9@gmail.com>
2021-04-16 11:49:17 -0400

    Added a leaderboard management form

--- github_leaderboard/app/forms.py
+++ github_leaderboard/app/forms.py
@@ -36,3 +36,37 @@ class CreateLeaderboardForm(forms.ModelForm):
         )
         self.helper.form_method = "post"
         self.helper.add_input(Submit("submit", "Create Leaderboard"))
+
+
+class ManageLeaderboardForm(forms.Form):
+    """Form used on website to manage and change
+    values of a leaderboard
+    """
+
+    class Meta:
+        model = Leaderboard
+        fields = ("name", "repo_url", "end", "start")
+
+    name = forms.CharField(required=False, max_length=255)
+    repo_url = forms.URLField(required=False)
+    start = forms.DateTimeField(required=False)
+    end = forms.DateTimeField(required=False)
+
+    COL_STYLE = "form-group mb-0 mx-auto"
+    FIELD_STYLE = "text-center"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Column(Field("name", css_class=self.FIELD_STYLE), css_class=self.COL_STYLE),
+            Column(
+                Field("repo_url", css_class=self.FIELD_STYLE), css_class=self.COL_STYLE
+            ),
+            Column(Field("end", css_class=self.FIELD_STYLE), css_class=self.COL_STYLE),
+            Column(
+                Field("start", css_class=self.FIELD_STYLE), css_class=self.COL_STYLE
+            ),
+        )
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Change Leaderboard"))
